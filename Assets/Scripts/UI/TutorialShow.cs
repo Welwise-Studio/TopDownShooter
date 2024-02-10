e56a3dfb94755fb0934ce4dd73d1d84de5540621
Be4Die <misha375375@gmail.com>
2024-02-10 19:41:04 +0400

    Update TutorialShow.cs

--- Assets/Scripts/UI/TutorialShow.cs
+++ Assets/Scripts/UI/TutorialShow.cs
@@ -6,18 +6,21 @@ public class TutorialShow : MonoBehaviour
 {
     public static bool IsShown;
 
+    private bool _currentShown;
+
     private void Start()
     {
         if (!IsShown)
         {
             Time.timeScale = .000000003f;
             IsShown = true;
+            _currentShown = true;
         }
     }
 
     private void Update()
     {
-        if (IsShown)
+        if (IsShown && !_currentShown)
             return;
 
         if (Input.anyKey || Input.touchCount > 0)
